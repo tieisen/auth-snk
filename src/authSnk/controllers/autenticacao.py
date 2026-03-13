c4d5fa0f588be1113edccb021b618eedcd47d47c
Rodrigo Vieira <rodrigo.vieira@grupoeisen.com.br>
2026-03-13 10:32:19 -0300

    adicionado response model

--- src/authSnk/controllers/autenticacao.py
+++ src/authSnk/controllers/autenticacao.py
@@ -5,11 +5,17 @@ from src.authSnk.services.autenticacao import AutenticacaoService
 
 class AutenticacaoLogarModel(BaseModel):
     solucaoId:int
+    
+class AutenticacaoResponse(BaseModel):
+    solucaoId: str
+    token: str
+    dhExpiracaoToken: str
+    mensagem: str
 
 router = APIRouter()
 header_scheme = APIKeyHeader(name="xToken")
 
-@router.post("/autenticar/{solucaoId}", status_code=status.HTTP_200_OK)
+@router.post("/autenticar/{solucaoId}", status_code=status.HTTP_200_OK, response_model=AutenticacaoResponse)
 async def autenticar(solucaoId:int, xToken: str = Depends(header_scheme)) -> dict:
     autenticacao_service = AutenticacaoService(solucaoId, xToken)
     sucesso, retorno = await autenticacao_service.autenticar()
